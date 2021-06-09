0cd8f2b903977b795a0a8b44468942427e71c424
strobosaur <strobosaur@gmail.com>
2021-06-09 10:44:15 +0200

    Multiple dice rolls & print sum

--- dice_roller.py
+++ dice_roller.py
@@ -1,8 +1,16 @@
 import random
+dice_rolls = 2
 
 def main():
-  roll = random.randint(1,6)
-  print(f'You rolled a {roll}')
+
+  dice_sum = 0
+
+  for i in range(0,dice_rolls):
+    roll = random.randint(1,6)
+    dice_sum += roll
+    print(f'You rolled a {roll}')
+
+  print(f'Dice roll total: {dice_sum}')
 
 if __name__== "__main__":
   main()
