b1ada1817a8e51257c418e8e8304873207f9a188
strobosaur <strobosaur@gmail.com>
2021-06-09 10:49:44 +0200

    Tutorial complete

--- dice_roller.py
+++ dice_roller.py
@@ -8,7 +8,12 @@ def main():
   for i in range(0,dice_rolls):
     roll = random.randint(1,6)
     dice_sum += roll
-    print(f'You rolled a {roll}')
+    if (roll == 1):
+      print(f'Ouch! You rolled a {roll}')
+    elif (roll == 6):
+      print(f'Nice! You rolled a {roll}')
+    else:
+      print(f'You rolled a {roll}')
 
   print(f'Dice roll total: {dice_sum}')
 
